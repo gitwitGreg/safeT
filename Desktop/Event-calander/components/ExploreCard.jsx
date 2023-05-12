bd0afc5a1049a578d63dea5afc58b17aed5b5b5b
Greg <Godly@udel.edu>
2023-05-12 15:04:28 -0400

    put in that work -Greg

--- Desktop/Event-calander/components/ExploreCard.jsx
+++ Desktop/Event-calander/components/ExploreCard.jsx
@@ -5,7 +5,7 @@ import { motion } from 'framer-motion';
 import styles from '../styles';
 import { fadeIn } from '../utils/motion';
 
-const ExploreCard = ({ id, imgUrl, index, active, handleClick }) => (
+const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
   <motion.div
     variants={fadeIn('right', 'spring', index * 0.5, 0.75)}
     className={`relative ${
@@ -20,7 +20,7 @@ const ExploreCard = ({ id, imgUrl, index, active, handleClick }) => (
     />
     {active !== id ? (
       <h3 className="font-semibold sm:text-[26px] text-[18px] text-white absolute z-0 lg:bottom-20 lg:rotate-[-90deg] lg:origin-[0,0]">
-        {}
+        {title}
       </h3>
     ) : (
       <div className="absolute bottom-0 p-8 flex justify-start w-full flex-col bg-[rgba(0,0,0,0.5)] rounded-b-[24px]">
@@ -34,10 +34,10 @@ const ExploreCard = ({ id, imgUrl, index, active, handleClick }) => (
           />
         </div>
         <p className="font-normal text-[16px] leading-[20.16px] text-white uppercase">
-          Explore
+          Enter Metaverse
         </p>
         <h2 className="mt-[24px] font-semibold sm:text-[32px] text-[24px] text-white">
-          {}
+          {title}
         </h2>
       </div>
     )}
--- Desktop/Event-calander/constants/index.js
+++ Desktop/Event-calander/constants/index.js
@@ -2,48 +2,48 @@ export const exploreWorlds = [
   {
     id: 'world-1',
     imgUrl: '/planet-01.png',
-    title: 'The Hogwarts',
+    title: 'Student Events',
   },
   {
     id: 'world-2',
     imgUrl: '/planet-02.png',
-    title: 'The Upside Down',
+    title: 'ALumni Events',
   },
   {
     id: 'world-3',
     imgUrl: '/planet-03.png',
-    title: 'Kadirojo Permai',
+    title: 'Athletic events',
   },
   {
     id: 'world-4',
     imgUrl: '/planet-04.png',
-    title: 'Paradise Island',
+    title: 'Prospective students',
   },
   {
     id: 'world-5',
     imgUrl: '/planet-05.png',
-    title: 'Hawkins Labs',
+    title: 'Whats Hot',
   },
 ];
 
 export const startingFeatures = [
-  'Find a world that suits you and you want to enter',
-  'Enter the world by reading basmalah to be safe',
+  'Find events that intrest you',
+  'Click the link and follow to the direct website',
   'No need to beat around the bush, just stay on the gas and have fun',
 ];
 
 export const newFeatures = [
   {
     imgUrl: '/vrpano.svg',
-    title: 'A new world',
+    title: 'A new way',
     subtitle:
-        'we have the latest update with new world for you to try never mind',
+        'we have the most accessible links',
   },
   {
     imgUrl: '/headset.svg',
-    title: 'More realistic',
+    title: 'custom search',
     subtitle:
-        'In the latest update, your eyes are narrow, making the world more realistic than ever',
+        'We take your searches and give you customized results',
   },
 ];
 
